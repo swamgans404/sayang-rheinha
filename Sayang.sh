fce9c59da439983eaf7498523d183c9899df3400
swamgans404 <48240248+swamgans404@users.noreply.github.com>
2019-03-05 16:14:38 +0700

    Create Sayang.sh

---
+++ Sayang.sh
@@ -0,0 +1,91 @@
+g#!/system/xbin/bash
+#Jangan di recode dong ster 
+#usaha kalo gk bisa nanya 
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'#!/system/xbin/bash
+#Jangan di recode dong ster 
+#usaha kalo gk bisa nanya 
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+echo $blue "Gw Tuh Sayang Ama Lu"
+sleep 1
+clear
+echo $green "Gw Tuh Sayang Ama Lu"
+sleep 1
+clear
+echo $yellow "Gw Tuh Sayang Ama Lu"
+sleep 1
+clear
+
+echo "Gw Itu Sayang Ama Lu Gw Gk mau jadi Teman Lu gw 
+Mau nya Lebih Dari Itu"|lolcat
+echo "
+Gw Ingin serius Ama Lu Tolong Terima Gw Apa Ada Nya Walapun 
+Gw Jlk Tapi Hati Gw gk Sejlk Itu Ko"|lolcat
+echo "Mau Kah Kamu Jadi PacarKu"|lolcat
+echo "1.ya"|lolcat
+echo "2.Tidak"|lolcat
+read -p "<?>:" gmt
+
+if [ $gmt = 1 ]
+then 
+clear
+echo $red "Gw Janji Bakal Bikin Lu Bahagia Semampu gw😊"
+echo $green "Makasi Udh Milih Gw Sebagai Pacar Lu"
+fi
+
+if [ $gmt = 2 ]
+then 
+clear
+echo $red "Kalo Emng Ini Ngebuat Hati Lu Bahagia Gpp ko"
+echo $green "Btw tq udh bikin gw bahagia"
+fi
+
+echo $blue "Gw Tuh Sayang Ama Lu"
+sleep 1
+clear
+echo $green "Gw Tuh Sayang Ama Lu"
+sleep 1
+clear
+echo $yellow "Gw Tuh Sayang Ama Lu"
+sleep 1
+bersih
+
+ "Gw Itu Sayang Ama Lu Gw Gk mau jadi Teman Lu gw 
+Mau nya Lebih Dari Itu"|lolcat
+echo "
+Gw Ingin serius Ama Lu Tolong Terima Gw Apa Ada Nya Walapun 
+Gw Jlk Tapi Hati Gw gk Sejlk Itu Ko"|lolcat
+echo "Mau Kah Kamu Jadi PacarKu"|lolcat
+echo "1.ya"|lolcat
+echo "2.Tidak"|lolcat
+read -p "<?>:" gmt
+
+if [ $gmt = 1 ]
+then 
+clear
+echo $red "Gw Janji Bakal Bikin Lu Bahagia Semampu gw😊"
+echo $green "Makasi Udh Milih Gw Sebagai Pacar Lu"
+fi
+
+if [ $gmt = 2 ]
+then 
+clear
+echo $red "Kalo Emng Ini Ngebuat Hati Lu Bahagia Gpp ko"
+echo $green "Btw tq udh bikin gw bahagia"
+fi
